3346d9f93f58410ef4d2bea538d5d5ce043a0d84
TOAH-X <ikeda2003512@gmail.com>
2026-06-09 15:33:54 +0900

    GameMangerSystemの一括管理

---
+++ Assets/Scripts/System/Singleton/GameBootstrapper.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace ParcaDefecta.System
+{
+    /// <summary>
+    /// ゲーム起動時に一度だけ実行される初期化クラス。
+    /// </summary>
+    public static class GameBootstrapper
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            // 各マネージャーの Instance を参照することで、Singleton.cs 側のロジックで
+            // 「個別のGameObject作成」と「共通の親への紐付け」を自動実行させます。
+            _ = TimeManager.Instance;
+            _ = PauseManager.Instance;
+            _ = FPSManager.Instance;
+            _ = SceneLoader.Instance;
+            _ = SoundManager.Instance;
+        }
+    }
+}
--- Assets/Scripts/System/Singleton/Singleton.cs
+++ Assets/Scripts/System/Singleton/Singleton.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+    private const string ContainerName = "GameManagerSystems";
+
     public static T Instance
     {
         get
@@ -14,6 +16,9 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
                 {
                     GameObject obj = new GameObject(typeof(T).Name);
                     instance = obj.AddComponent<T>();
+
+                    // 生成時に自動で親に紐付ける
+                    SetupParent(obj);
                 }
             }
             return instance;
@@ -25,11 +30,31 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         if (instance == null)
         {
             instance = this as T;
-            DontDestroyOnLoad(gameObject);
+
+            // シーンに最初から置いてある場合も親に紐付ける
+            SetupParent(gameObject);
+
+            // 親が DontDestroyOnLoad なら子も自動的に維持されるが、
+            // 念のためルートオブジェクトに対してのみ実行する
+            if (transform.parent == null)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+
+    private static void SetupParent(GameObject child)
+    {
+        GameObject container = GameObject.Find(ContainerName);
+        if (container == null)
+        {
+            container = new GameObject(ContainerName);
+            DontDestroyOnLoad(container);
+        }
+        child.transform.SetParent(container.transform);
     }
 }
